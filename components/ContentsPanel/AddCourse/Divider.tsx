26c99ea5cc2add9a18199a7e281fa92bbf562d06
theduv <tduval@student.42.fr>
2023-10-04 14:05:01 +0200

    you can submit multiple links when adding a course but its currently really ugly

---
+++ components/ContentsPanel/AddCourse/Divider.tsx
@@ -0,0 +1,7 @@
+const Divider = () => {
+  return (
+    <div className="border border-t-0 border-l-0 border-r-0 w-full border-white opacity-20" />
+  );
+};
+
+export default Divider;
--- components/ContentsPanel/AddCourse/main.tsx
+++ components/ContentsPanel/AddCourse/main.tsx
@@ -10,6 +10,7 @@ import clsx from "clsx";
 import { addDoc, collection } from "firebase/firestore";
 import { useEffect, useRef, useState } from "react";
 import { HexColorPicker } from "react-colorful";
+import Divider from "./Divider";
 
 const AddCourse = () => {
   const panelStore: Store = useRightPanelStore((state: any) => ({ ...state }));
@@ -24,9 +25,15 @@ const AddCourse = () => {
   const [colorBackground, setColorBackground] = useState("#FFFFFF");
   const [pickText, setPickText] = useState<boolean>(false);
   const [pickBackground, setPickBackground] = useState<boolean>(false);
-  const [link, setLink] = useState<Array<string>>([]);
+  const [links, setLinks] = useState<Array<string>>([]);
+  const [linkValue, setLinkValue] = useState<string>("");
   const refTextPicker = useRef<HTMLDivElement>(null);
   const refBackgroundPicker = useRef<HTMLDivElement>(null);
+
+  const onClickAddLink = () => {
+    setLinks((oldLinks) => [...oldLinks, linkValue]);
+    setLinkValue("");
+  };
 
   useEffect(() => {
     setDate(panelStore.addCourseDefault.date);
@@ -55,8 +62,8 @@ const AddCourse = () => {
     };
   }, [refBackgroundPicker]);
 
-  const onChangeLink = (e: any) => {
-    setLink(e.target.value);
+  const onChangeLinkValue = (e: any) => {
+    setLinkValue(e.target.value);
   };
 
   const onSubmitForm = async (e: any) => {
@@ -83,12 +90,13 @@ const AddCourse = () => {
         timeTo: JSON.stringify(dateEnd),
         backgroundColor: colorBackground,
         textColor: colorText,
-        links: link.length !== 0 ? `${link};` : "",
+        links: links.join(";"),
       });
 
       setTitle("");
       setNotes("");
       toast("Cours correctement ajouté");
+      setLinks([]);
     } catch (e) {
       console.log(e);
     }
@@ -98,7 +106,7 @@ const AddCourse = () => {
     <div>
       <form
         onSubmit={onSubmitForm}
-        className="flex flex-col items-center justify-center space-y-2   p-4"
+        className="flex flex-col  justify-center space-y-2 p-4"
       >
         <CustomInput
           type="text"
@@ -106,12 +114,15 @@ const AddCourse = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         ></CustomInput>
+        <Divider />
         <CustomInput
           placeholder="Date du cours"
           value={date}
           onChange={(e) => setDate(e.target.value)}
           type="date"
         />
+        <Divider />
+
         <CustomInput
           value={timeStart}
           onChange={(e) => setTimeStart(e.target.value)}
@@ -119,6 +130,7 @@ const AddCourse = () => {
           type="time"
           step="3600"
         />
+        <Divider />
         <CustomInput
           value={timeEnd}
           onChange={(e) => setTimeEnd(e.target.value)}
@@ -130,18 +142,40 @@ const AddCourse = () => {
           value={notes}
           onChange={(e) => setNotes(e.target.value)}
           placeholder="Notes additionnelles (prof absent, changement de salle...)"
-          className="rounded-lg bg-gray-600 p-4 w-full h-64"
+          className="rounded-lg bg-gray-600 p-4 w-full h-32"
         />
-        <label className="flex w-full justify-between items-center space-x-3">
-          <h2>Lien prise de note</h2>
-          <input
-            value={link}
-            onChange={onChangeLink}
-            type="url"
-            className="rounded-lg bg-gray-600 px-4 py-2"
-            placeholder="Lien google drive, dropbox..."
-          />
-        </label>
+        <Divider />
+        <div className="flex flex-col space-y-4">
+          <h2>Lien(s) de prise de notes</h2>
+          {links.length !== 0 ? (
+            links.map((link) => (
+              <a
+                className="text-blue-600 underline text-sm"
+                href="link"
+                key={`Link to ${link}`}
+              >
+                {link}
+              </a>
+            ))
+          ) : (
+            <h4 className="italic text-sm">
+              Aucun lien n'a encore été posté pour ce cours.
+            </h4>
+          )}
+          <label className="flex w-full justify-between items-center space-x-3">
+            <input
+              value={linkValue}
+              onChange={onChangeLinkValue}
+              type="url"
+              className="rounded-lg bg-gray-600 px-4 py-2 w-4/5"
+              placeholder="Lien google drive, dropbox..."
+            />
+            <button className="text-2xl" onClick={onClickAddLink} type="button">
+              +
+            </button>
+          </label>
+        </div>
+        <Divider />
         <div className="flex w-full justify-between items-center">
           <div className="flex flex-col">
             <div className="relative">
@@ -185,14 +219,16 @@ const AddCourse = () => {
             Example
           </div>
         </div>
+        <Divider />
         <button
+          disabled={title.length === 0}
           type="submit"
-          className={clsx("rounded-lg px-6 py-2", {
+          className={clsx("rounded-lg w-1/2 px-6 py-2", {
             "bg-gray-300 cursor-default text-gray-400": title.length === 0,
             "bg-blue-600": title.length !== 0,
           })}
         >
-          Ajouter
+          Ajouter le cours
         </button>
       </form>
     </div>
