c97560240c66bfb793b4d37f5157346345ef40de
theduv <tduval@student.42.fr>
2023-10-07 22:31:03 +0200

    added env file

--- firebase/firebaseInit.ts
+++ firebase/firebaseInit.ts
@@ -2,16 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJoRB4ayYk77jCV6Gn66p2te30bjUvJTY",
-  authDomain: "sdlcourses.firebaseapp.com",
-  projectId: "sdlcourses",
-  storageBucket: "sdlcourses.appspot.com",
-  messagingSenderId: "628257581911",
-  appId: "1:628257581911:web:014883d767710bf650ec12",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
+
+console.log(process.env.FIREBASE_APP_ID);
 
 export default db;
