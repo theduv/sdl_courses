7a5ef8b9c07c0a5d3363f1c6b86a2a43e5c4c93c
theduv <tduval@student.42.fr>
2023-10-07 20:43:15 +0200

    border on panel header, border on panel

--- components/RightPanel/RightPanelHeader.tsx
+++ components/RightPanel/RightPanelHeader.tsx
@@ -15,7 +15,7 @@ const RightPanelHeader = () => {
   };
 
   return (
-    <div className="w-full flex items-center p-4 z-10 justify-between sticky bg-gray-800 top-0">
+    <div className="w-full flex border border-t-0 border-l-0 border-r-0 border-gray-600 border-2 items-center p-4 z-10 justify-between sticky bg-gray-800 top-0">
       <div />
       <h1 className="text-2xl font-bold">{getTitle()}</h1>
       <button onClick={() => storePanel.setOpen(false)}>X</button>
